6b67855d6252d69c8145db9afdb327b2c5e9d0c0
SefaKILIÇ <sefakkilic@gmail.com>
2024-07-09 15:02:02 +0300

    Player Controller
    Player controller ve body eklendi. temel item alma ve item detayları için uı eklendi

---
+++ Assets/Scripts/Player/AnimatorControls.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using UnityEngine;
+using TMPro;
+
+namespace Player
+{
+    
+    public class AnimatorControls : MonoBehaviour
+    {
+        private Animator _anim;
+        [SerializeField] private GameObject _key;
+        [SerializeField] private TextMeshProUGUI _itemInfoText;
+        
+        private void Awake()
+        {
+
+            _anim = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            RaycastSource();
+        }
+
+        void RaycastSource()
+        {
+            _anim.SetBool("isPicking", true);
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+			
+            if(Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.CompareTag("item"))
+                {
+                    _itemInfoText.text = hit.collider.name;
+                    if (Input.GetKeyDown(KeyCode.E))
+                    {
+                        TakeItem();
+                        Destroy(hit.collider.gameObject);
+                    }
+                }else _itemInfoText.text = "";
+
+                
+            }
+            
+
+            if (Input.GetKeyUp(KeyCode.E))
+            {
+                _anim.SetBool("isPicking", false);
+            }
+        }
+
+        public void TakeItem()
+        {
+            _key.SetActive(true);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player/CharacterCrouch.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class CharacterCrouch : MonoBehaviour
+{
+    public float crouchHeight = 1.0f;
+    public float normalHeight = 2.0f;
+    public float crouchSpeed = 5.0f;
+    private CapsuleCollider characterCollider;
+    private bool isCrouching = false;
+
+    void Start()
+    {
+        characterCollider = GetComponent<CapsuleCollider>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            isCrouching = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            isCrouching = false;
+        }
+
+        float targetHeight = isCrouching ? crouchHeight : normalHeight;
+        characterCollider.height = Mathf.Lerp(characterCollider.height, targetHeight, Time.deltaTime * crouchSpeed);
+
+        Vector3 scale = transform.localScale;
+        scale.y = characterCollider.height / normalHeight;
+        transform.localScale = scale;
+    }
+}
