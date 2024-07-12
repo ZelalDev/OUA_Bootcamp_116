8449dd42ec3674e13d923afed2d4ccdae390db6f
SefaKILIÇ <sefakkilic@gmail.com>
2024-07-12 20:57:38 +0300

    outline
    örnek item ve outline eklendi

--- Assets/Scripts/Player/AnimatorControls.cs
+++ Assets/Scripts/Player/AnimatorControls.cs
@@ -4,16 +4,16 @@ using TMPro;
 
 namespace Player
 {
-    
     public class AnimatorControls : MonoBehaviour
     {
         private Animator _anim;
         [SerializeField] private GameObject _key;
         [SerializeField] private TextMeshProUGUI _itemInfoText;
-        
+
+        private Outline currentOutline; // Mevcut Outline bileşeni
+
         private void Awake()
         {
-
             _anim = GetComponent<Animator>();
         }
 
@@ -28,22 +28,42 @@ namespace Player
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
             RaycastHit hit;
-			
-            if(Physics.Raycast(ray, out hit))
+
+            if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.CompareTag("item"))
                 {
                     _itemInfoText.text = hit.collider.name;
+
+                    // Mevcut Outline bileşenini kontrol et ve ekle
+                    Outline outline = hit.collider.gameObject.GetComponent<Outline>();
+                    if (outline == null)
+                    {
+                        outline = hit.collider.gameObject.AddComponent<Outline>();
+                        outline.OutlineMode = Outline.Mode.OutlineAll;
+                        outline.OutlineColor = Color.white;
+                        outline.OutlineWidth = 2f;
+                    }
+
+                    currentOutline = outline;
+
                     if (Input.GetKeyDown(KeyCode.E))
                     {
                         TakeItem();
                         Destroy(hit.collider.gameObject);
+                        currentOutline = null; // Mevcut Outline bileşeni yok et
                     }
-                }else _itemInfoText.text = "";
-
-                
+                }
+                else
+                {
+                    _itemInfoText.text = "";
+                    RemoveCurrentOutline();
+                }
             }
-            
+            else
+            {
+                RemoveCurrentOutline();
+            }
 
             if (Input.GetKeyUp(KeyCode.E))
             {
@@ -51,10 +71,18 @@ namespace Player
             }
         }
 
+        void RemoveCurrentOutline()
+        {
+            if (currentOutline != null)
+            {
+                Destroy(currentOutline);
+                currentOutline = null;
+            }
+        }
+
         public void TakeItem()
         {
             _key.SetActive(true);
         }
-
     }
 }
